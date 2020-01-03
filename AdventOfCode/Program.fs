8bd2d3b2d0a151cfd86db96251dbcdfe748a3351
Thomas GOETZMANN <thomas.goetzmann@gmail.com>
2020-01-03 20:30:52 +0100

    Solve Day 3 Part 1
    Solution has duplicate code and uses one mutable -> needs improvement

--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -2,6 +2,7 @@
 
 open Year2019Day1
 open Year2019Day2
+open Year2019Day3
 open Year2019Day4
 
 [<EntryPoint>]
@@ -15,7 +16,7 @@ let main argv =
     printfn "Day 2 part 1 solution is %i" SolveDay2Part1
     printfn "Day 2 part 2 solution is %A" SolveDay2Part2
     printfn "---"
-
+    printfn "Day 3 part 1 solution is %A" SolveDay3Part1
     printfn "---"
     printfn "Day 4 part 1 solution is %i" SolveDay4Part1
     printfn "Day 4 part 2 solution is %i" SolveDay4Part2
--- AdventOfCode/Year2019Day3.fs
+++ AdventOfCode/Year2019Day3.fs
@@ -3,10 +3,64 @@
 open System.IO
 
 let inputs = File.ReadAllLines "../../../inputs/input-year2019day3.txt"
+let testInputs = ["R8,U5,L5,D3"]
 
-let parseToArray (text:string) = text.Split(",") |> Seq.toArray
+type Direction = L | U | R | D
+type Move = { Length:int; Direction:Direction}
+type Coordinate = {X:int; Y:int}
 
-let coordinates1 = inputs.[0] |> parseToArray
-let coordinates2 = inputs.[1] |> parseToArray
+let parseDir = 
+  function
+  | 'L' -> L
+  | 'U' -> U
+  | 'R' -> R
+  | 'D' -> D
+  | invalidDirectionChar -> failwithf "Invalid direction: %c" invalidDirectionChar
 
+let parseMove (input:string) = 
+  { 
+    Length = int input.[1..];
+    Direction = parseDir input.[0]
+  }
 
+let parse (text:string) = text.Split(",") |> Seq.map parseMove
+
+let drawWireSegment (coordinates:List<Coordinate>, move:Move) = 
+  let rec addMove (coordinates:List<Coordinate>, position:Coordinate, move:Move) =
+    match move with
+    | m when m.Length <= 0 -> coordinates
+    | m -> 
+      match m.Direction with 
+      | L -> 
+        let newCoordinate = { X = position.X - 1; Y = position.Y }
+        addMove (newCoordinate::coordinates, newCoordinate, {Length = m.Length - 1; Direction = m.Direction})
+      | R -> 
+        let newCoordinate = { X = position.X + 1; Y = position.Y }
+        addMove (newCoordinate::coordinates, newCoordinate, {Length = m.Length - 1; Direction = m.Direction})
+      | U -> 
+        let newCoordinate = { X = position.X; Y = position.Y + 1 }  
+        addMove (newCoordinate::coordinates, newCoordinate, {Length = m.Length - 1; Direction = m.Direction})
+      | D -> 
+        let newCoordinate = { X = position.X; Y = position.Y - 1 }
+        addMove (newCoordinate::coordinates, newCoordinate, {Length = m.Length - 1; Direction = m.Direction})
+  addMove (coordinates, coordinates.Head, move)
+
+let drawWire (moves:seq<Move>) = 
+  let mutable coordinates = [ {X=0; Y=0} ]
+  for move in moves do
+    coordinates <- drawWireSegment(coordinates,move)
+  coordinates
+
+let ManhattanDistance coordinate = abs coordinate.X + abs coordinate.Y
+
+let SolveDay3Part1 = 
+  inputs 
+  |> Seq.map parse 
+  |> Seq.map drawWire
+  |> Seq.map Set.ofList
+  |> Set.intersectMany
+  |> Set.map ManhattanDistance
+  |> Set.toSeq
+  |> Seq.filter (fun x -> x <> 0) //removes the starting point which is also considered as a intersection point
+  |> Seq.min
+  
